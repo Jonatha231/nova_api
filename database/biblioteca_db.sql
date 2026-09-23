66fc587f25f99d419b5b457e9622d3a5f2a53572
Jonatha231 <jowwjoww022@gmail.com>
2026-09-23 16:08:48 -0300

    Etapa 3 - docs: confirma contrato da api para frontend

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23/09/2026 às 20:56
+-- Tempo de geração: 23/09/2026 às 21:07
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -60,7 +60,7 @@ ALTER TABLE `livros`
 -- AUTO_INCREMENT de tabela `livros`
 --
 ALTER TABLE `livros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
